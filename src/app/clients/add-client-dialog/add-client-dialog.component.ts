0efa74a56b5405fca6482c19aff99f09c98babd4
Yasin <ergaibi.yassine@gmail.com>
2020-12-05 14:51:01 +0100

    clients service

---
+++ src/app/clients/add-client-dialog/add-client-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-client-dialog',
+  templateUrl: './add-client-dialog.component.html',
+  styleUrls: ['./add-client-dialog.component.scss']
+})
+export class AddClientDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
